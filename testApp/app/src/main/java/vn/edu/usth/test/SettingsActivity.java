b28b362ee4df4a664dc28594dfb3d69041b3be16
QK04 <khanhnq.22bi13211@usth.edu.vn>
2024-10-09 21:15:13 +0700

    can change the language system but the app doesn't work well

--- testApp/app/src/main/java/vn/edu/usth/test/SettingsActivity.java
+++ testApp/app/src/main/java/vn/edu/usth/test/SettingsActivity.java
@@ -1,11 +1,19 @@
 package vn.edu.usth.test;
 
+
 import android.content.Intent;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -16,22 +24,41 @@ import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import java.util.Locale;
+
 public class SettingsActivity extends AppCompatActivity {
+    public static final String[] languages = {"Select","English", "Tiếng Việt"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        // Change color of Language Spinner
         Spinner languageSpinner = findViewById(R.id.language_select);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.languages, R.layout.spinner_item); // Use the custom layout
-
-        adapter.setDropDownViewResource(R.layout.spinner_item); // Set for the dropdown view
+        // Set up the adapter for the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         languageSpinner.setAdapter(adapter);
-        languageSpinner.setPopupBackgroundResource(R.color.white);
 
+        // Set up the listener for the spinner
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    // Change to English
+                    setLocale("en");
+                } else if (position == 2) {
+                    // Change to Vietnamese
+                    setLocale("vi");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         SwitchCompat mySwitch = findViewById(R.id.switch_notifications);
         // Set the default color to red
@@ -46,38 +73,43 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             }
         });
-        //Set up toolbar
+
+        // Set up the toolbar
         Toolbar toolbar = findViewById(R.id.backButton);
         setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
-        // Delete the title in the bar
-        actionBar.setDisplayShowTitleEnabled(false);
-        // Create the back button and return the page before
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-        toolbar.setNavigationIcon(R.drawable.arrow_back);
-        Drawable drawable = toolbar.getNavigationIcon();
-        if (drawable != null) {
-            drawable.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_ATOP);
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationIcon(R.drawable.arrow_back);
+            Drawable drawable = toolbar.getNavigationIcon();
+            if (drawable != null) {
+                drawable.setColorFilter(ContextCompat.getColor(this, android.R.color.white), PorterDuff.Mode.SRC_ATOP);
+            }
         }
 
-
-        // Click on account setting to login (account) activity
-        LinearLayout account_setting = findViewById(R.id.account_setting_layout);
-
-        account_setting.setOnClickListener(view -> {
+        // Click on account setting to navigate to the profile activity
+        LinearLayout accountSettingLayout = findViewById(R.id.account_setting_layout);
+        accountSettingLayout.setOnClickListener(view -> {
             Intent intent = new Intent(SettingsActivity.this, ProfileActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
             startActivity(intent);
         });
     }
 
+    private void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // In actionbar, this is default home, not any id. It presents for "back" or "up" button
         if (item.getItemId() == android.R.id.home) {
-            // Turn back the previous page
             finish();
             return true;
         }
